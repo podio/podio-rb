2aa33b9565621f042522e63d3366985e114a0df6
Florian Munz <surf@theflow.de>
2010-11-04 10:43:30 +0100

    handle 400 status responses

--- lib/podio/middleware/error_response.rb
+++ lib/podio/middleware/error_response.rb
@@ -6,6 +6,8 @@ module Podio
       def self.register_on_complete(env)
         env[:response].on_complete do |finished_env|
           case finished_env[:status]
+            when 400
+              raise Error::AuthorizationError, finished_env[:body].inspect
             when 401
               if finished_env[:body]['error'] == 'Unauthorized: expired_token'
                 raise Error::TokenExpired, finished_env[:body].inspect
