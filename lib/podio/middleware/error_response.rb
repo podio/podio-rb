2de8b0d8c2b8614e5d6c99ca7c3d1c8999d3c2d7
Florian Munz <surf@theflow.de>
2010-11-30 17:49:48 +0100

    better error reporting

--- lib/podio/middleware/error_response.rb
+++ lib/podio/middleware/error_response.rb
@@ -7,21 +7,21 @@ module Podio
         env[:response].on_complete do |finished_env|
           case finished_env[:status]
             when 400
-              raise Error::AuthorizationError, finished_env[:body].inspect
+              raise Error::BadRequestError, finished_env[:body].error
             when 401
               if finished_env[:body]['error'] == 'Unauthorized: expired_token'
-                raise Error::TokenExpired, finished_env[:body].inspect
+                raise Error::TokenExpired, finished_env[:body].error
               else
-                raise Error::AuthorizationError, finished_env[:body].inspect
+                raise Error::AuthorizationError, finished_env[:body].error
               end
             when 403
-              raise Error::AuthorizationError, finished_env[:body].inspect
+              raise Error::AuthorizationError, finished_env[:body].error
             when 404
               raise Error::NotFoundError, "#{finished_env[:method].to_s.upcase} #{finished_env[:url]}"
             when 410
               raise Error::GoneError, "#{finished_env[:method].to_s.upcase} #{finished_env[:url]}"
             when 500
-              raise Error::ServerError, finished_env[:body].inspect
+              raise Error::ServerError, finished_env[:body].error
           end
         end
       end
