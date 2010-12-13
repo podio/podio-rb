84583452e76d81a3f9ad15840840e504871f1f5e
Florian Munz <surf@theflow.de>
2010-12-13 16:39:00 +0100

    the error response format changed again

--- lib/podio/middleware/error_response.rb
+++ lib/podio/middleware/error_response.rb
@@ -7,21 +7,21 @@ module Podio
         env[:response].on_complete do |finished_env|
           case finished_env[:status]
             when 400
-              raise Error::BadRequestError, finished_env[:body]['error']
+              raise Error::BadRequestError, finished_env[:body].inspect
             when 401
-              if finished_env[:body]['error'] =~ /expired_token/
-                raise Error::TokenExpired, finished_env[:body]['error']
+              if finished_env[:body]['error_description'] =~ /expired_token/
+                raise Error::TokenExpired, finished_env[:body].inspect
               else
-                raise Error::AuthorizationError, finished_env[:body]['error']
+                raise Error::AuthorizationError, finished_env[:body].inspect
               end
             when 403
-              raise Error::AuthorizationError, finished_env[:body]['error']
+              raise Error::AuthorizationError, finished_env[:body].inspect
             when 404
               raise Error::NotFoundError, "#{finished_env[:method].to_s.upcase} #{finished_env[:url]}"
             when 410
               raise Error::GoneError, "#{finished_env[:method].to_s.upcase} #{finished_env[:url]}"
             when 500
-              raise Error::ServerError, finished_env[:body]['error']
+              raise Error::ServerError, finished_env[:body].inspect
           end
         end
       end
