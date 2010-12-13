b8bf9114fbea1bb423b7fc5c94c6886b8f73d230
Florian Munz <surf@theflow.de>
2010-12-13 14:29:08 +0100

    new modules for handling Contacts and Spaces

---
+++ lib/podio/contact.rb
@@ -0,0 +1,19 @@
+module Podio
+  module Contact
+    include Podio::ResponseWrapper
+    extend self
+
+    def find(id)
+      member Podio.connection.get("/space/{id}").body
+    end
+
+    def find_all_for_org(org_id, options={})
+      options.assert_valid_keys(:key, :value, :limit, :offset, :type, :order)
+      options[:type] ||= 'full'
+
+      list Podio.connection.get { |req|
+        req.url("/contact/org/#{org_id}", options)
+      }.body
+    end
+  end
+end
---
+++ lib/podio/space.rb
@@ -0,0 +1,14 @@
+module Podio
+  module Space
+    include Podio::ResponseWrapper
+    extend self
+
+    def find(id)
+      member Podio.connection.get("/space/{id}").body
+    end
+
+    def find_all_for_org(org_id)
+      list Podio.connection.get("/org/#{org_id}/space/").body
+    end
+  end
+end
