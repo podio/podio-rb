857bd4faaab46e8d9942dce8a8d19083cfb5cb2b
Florian Munz <surf@theflow.de>
2010-12-21 11:52:29 +0100

    add update and delete for organizations

--- lib/podio/organization.rb
+++ lib/podio/organization.rb
@@ -3,6 +3,19 @@ module Podio
     include Podio::ResponseWrapper
     extend self
 
+    def update(id, attributes)
+      response = Podio.connection.put do |req|
+        req.url "/org/#{id}"
+        req.body = attributes
+      end
+
+      response.status
+    end
+
+    def delete(id)
+      Podio.connection.delete("/org/#{id}").status
+    end
+
     def find(id)
       member Podio.connection.get("/org/#{id}").body
     end
