cd54a24ba11e6d9e70fbf3d60ddbc27c3b6b33db
Florian Munz <surf@theflow.de>
2010-12-29 16:05:27 +0100

    make it possible to login as a different user in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,8 +12,7 @@ class ActiveSupport::TestCase
   setup :set_podio_client
   setup :stub_responses if ENABLE_STUBS
 
-  def set_podio_client
-    token = Podio::OAuthToken.new('access_token' => '352e85cd67eff86179194515b91a75404c1169ad083ad435b200af834b9121665b2aaf894f599b7d9b1bee6b7551f3a11e2a02dc43def9a9b549b1f2a4fe9a42', 'refresh_token' => '82e7a11ae187f28a25261599aa6229cd89f8faee48cba18a75d70efae88ba665ced11d43143b7f5bebb31a4103662b851dd2db1879a3947b843259479fccfad3', 'expires_in' => 3600)
+  def set_podio_client(user_id = 1)
     Podio.client = Podio::Client.new(
       :api_url      => 'http://127.0.0.1:8080',
       :api_key      => 'dev@hoisthq.com',
@@ -21,8 +20,12 @@ class ActiveSupport::TestCase
       :enable_stubs => ENABLE_STUBS && !ENABLE_RECORD,
       :record_mode  => ENABLE_RECORD,
       :test_mode    => true,
-      :oauth_token  => token
+      :oauth_token  => new_token_for_user(user_id)
     )
+  end
+
+  def login_as_user(user_id)
+    set_podio_client(user_id)
   end
 
   def stub_responses
@@ -39,5 +42,18 @@ class ActiveSupport::TestCase
 
       Podio.client.stubs.send(method, url) { rack_response }
     end
+  end
+
+  def new_token_for_user(user_id)
+    tokens = {
+      1 => '352e85cd67eff86179194515b91a75404c1169ad083ad435b200af834b9121665b2aaf894f599b7d9b1bee6b7551f3a11e2a02dc43def9a9b549b1f2a4fe9a42',
+      2 => '58ac6854ac3e8d7e8797ed5b37ba8dfeca4224cabcd4ad4f04a5f328c83d9edb3be911e6fbccf742841369734ab26136c66380385e58ff2b5da03f7fe45852b1'
+    }
+
+    Podio::OAuthToken.new(
+      'access_token' => tokens[user_id],
+      'refresh_token' => '82e7a11ae187f28a25261599aa6229cd89f8faee48cba18a75d70efae88ba665ced11d43143b7f5bebb31a4103662b851dd2db1879a3947b843259479fccfad3',
+      'expires_in' => 3600
+    )
   end
 end
