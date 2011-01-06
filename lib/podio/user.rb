ed1bf6295d0ac30f0ea58cf4815593a378001c43
Casper Fabricius <me@casperfabricius.com>
2011-01-06 14:11:59 +0100

    Added method for fetching current user using the user/status API call

--- lib/podio/user.rb
+++ lib/podio/user.rb
@@ -2,5 +2,10 @@ module Podio
   module User
     include Podio::ResponseWrapper
     extend self
+
+    def current
+      member Podio.connection.get("/user/status").body['user']
+    end
+
   end
 end
