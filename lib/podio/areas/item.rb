e4ed55187dc7c18bb74459684064146cd6acda5b
Casper Fabricius <me@casperfabricius.com>
2011-01-14 17:01:52 +0100

    Added create method to Item

--- lib/podio/areas/item.rb
+++ lib/podio/areas/item.rb
@@ -10,5 +10,14 @@ module Podio
     def find_all_by_external_id(app_id, external_id)
       collection Podio.connection.get("/item/app/#{app_id}/v2/?external_id=#{external_id}").body
     end
+    
+    def create(app_id, attributes)
+      response = Podio.connection.post do |req|
+        req.url "/item/app/#{app_id}/"
+        req.body = attributes
+      end
+
+      response.body['item_id']
+    end    
   end
 end
