5dcacce1eaaadb70d37501b72c701322443579ca
Casper Fabricius <me@casperfabricius.com>
2011-02-02 17:42:08 +0100

    Added Conversation area

---
+++ lib/podio/areas/conversation.rb
@@ -0,0 +1,32 @@
+module Podio
+  module Conversation
+    include Podio::ResponseWrapper
+    extend self
+
+    def find(conversation_id)
+      member Podio.connection.get("/conversation/#{conversation_id}").body
+    end
+
+    def find_all_for_reference(ref_type, ref_id)
+      list Podio.connection.get("/conversation/#{ref_type}/#{ref_id}/").body
+    end
+
+    def create(attributes)
+      response = Podio.connection.post do |req|
+        req.url '/conversation/'
+        req.body = attributes
+      end
+      response.body
+    end
+
+    def create_for_reference(ref_type, ref_id, attributes)
+      response = Podio.connection.post do |req|
+        req.url "/conversation/#{ref_type}/#{ref_id}/"
+        req.body = attributes
+      end
+
+      response.body
+    end
+
+  end
+end
