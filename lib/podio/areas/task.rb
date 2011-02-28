2531aedd0f45a1058e2c7457b772871c517b986e
Florian Munz <surf@theflow.de>
2011-02-28 16:15:58 +0100

    support for updating a task

--- lib/podio/areas/task.rb
+++ lib/podio/areas/task.rb
@@ -21,6 +21,26 @@ module Podio
       response.body['task_id']
     end
 
+    def update_description(id, description)
+      Podio.connection.put("/task/#{id}/description", {:description => description}).status
+    end
+
+    def update_text(id, text)
+      Podio.connection.put("/task/#{id}/text", {:text => text}).status
+    end
+
+    def update_private(id, private_flag)
+      Podio.connection.put("/task/#{id}/private", {:private => private_flag}).status
+    end
+
+    def update_due_date(id, due_date)
+      Podio.connection.put("/task/#{id}/due_date", {:due_date => due_date}).status
+    end
+
+    def update_assignee(id, user_id)
+      Podio.connection.post("/task/#{id}/assign", {:responsible => user_id}).status
+    end
+
     def delete(id)
       Podio.connection.delete("/task/#{id}").status
     end
