beef804f4f1118a5b8d128c7621e132e65654b38
Kenneth Auchenberg <kenneth@auchenberg.dk>
2011-04-14 10:04:25 +0200

    Merged store.rb and app_store.rb

--- lib/podio/areas/app_store.rb
+++ lib/podio/areas/app_store.rb
@@ -42,4 +42,19 @@ module Podio
       Struct.new(:functional, :vertical).new(functionals, verticals)
     end
   end
+
+  module AppStoreShare
+    include Podio::ResponseWrapper
+    extend self
+
+    def find(id)
+      member Podio.connection.get("/app_store/#{id}/v2").body
+    end
+
+    def find_all_private_for_org(org_id)
+      list Podio.connection.get("/app_store/org/#{org_id}/").body['shares']
+    end
+
+  end
+
 end
