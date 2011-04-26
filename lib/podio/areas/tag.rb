0c68367f889269dac462ec7faa26f162b7a93cd4
Kenneth Auchenberg <kenneth@auchenberg.dk>
2011-04-26 16:11:18 +0200

    Added tagging functionality

--- lib/podio/areas/tag.rb
+++ lib/podio/areas/tag.rb
@@ -3,12 +3,21 @@ module Podio
     include Podio::ResponseWrapper
     extend self
 
+    def create(tagable_type, tagable_id, attributes)
+      response = Podio.connection.post do |req|
+        req.url "/tag/#{tagable_type}/#{tagable_id}/"
+        req.body = attributes
+      end
+
+      response.body
+    end
+
     def find_by_app(app_id, limit, text)
       list Podio.connection.get("/tag/app/#{app_id}/?limit=#{limit}&text=#{text}").body
     end
 
     def find_top_by_app(app_id, limit, text)
-      list Podio.connection.get("/tag/app/#{app_id}/top/?limit=#{limit}&text=#{text}").body
+      Podio.connection.get("/tag/app/#{app_id}/top/?limit=#{limit}&text=#{text}").body
     end
 
   end
