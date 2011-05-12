c755e48adfe215711e3812d84b806ca353c084fd
Florian Munz <surf@theflow.de>
2011-05-12 16:31:42 +0200

    support for status creation

--- lib/podio/areas/status.rb
+++ lib/podio/areas/status.rb
@@ -6,5 +6,14 @@ module Podio
     def find(id)
       member Podio.connection.get("/status/#{id}").body
     end
+
+    def create(space_id, attributes)
+      response = Podio.connection.post do |req|
+        req.url "/status/space/#{space_id}/"
+        req.body = attributes
+      end
+
+      response.body['status_id']
+    end
   end
 end
