e6e2b4ba54ff716ae48377cf4efc1a699b4420ec
Nick Barnwell <nickbarnwell@boltoncomputing.com>
2011-05-25 16:41:02 +0200

    Update news area

--- lib/podio/areas/news.rb
+++ lib/podio/areas/news.rb
@@ -30,11 +30,20 @@ module Podio
 		end
 
 		def find_all
-			list  Podio.connection.get("/news/?show_drafts=1").body
+			list Podio.connection.get("/news/admin/").body
 		end
 
 		def find_all_by_locale(locale)
 			list Podio.connection.get('/news/?locale=#{locale}').body
 		end
+
+		def find_all_by_target_group(target_group)
+			list Podio.connection.get('/news/?target_group=#{target_group}').body
+		end
+
+		def find_all_by_locale_and_group(locale, group)
+			list Podio.connection.get('/news/?locale=#{locale}?target_group=#{group}').body
+		end
+
 	end
 end
