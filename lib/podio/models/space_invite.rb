2317289e79b22bc59dc72f1187bdda1aef7bc171
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2011-07-18 15:10:53 +0200

    Adding methods for added members to spaces

--- lib/podio/models/space_invite.rb
+++ lib/podio/models/space_invite.rb
@@ -15,16 +15,29 @@ class Podio::SpaceInvite < ActivePodio::Base
     self.class.create(self.space_id, self.role, self.attributes.except(:contacts))
   end
 
+  def save_member
+    self.class.create_member(self.space_id, self.role, self.attributes.except(:contacts))
+  end
+
   def accept(invite_code)
     self.class.accept(invite_code)
   end
   
-  handle_api_errors_for :save, :accept # Call must be made after the methods to handle have been defined
+  handle_api_errors_for :save, :save_member, :accept # Call must be made after the methods to handle have been defined
   
   class << self
     def create(space_id, role, attributes={})
       response = Podio.connection.post do |req|
         req.url "/space/#{space_id}/invite"
+        req.body = attributes.merge(:role => role)
+      end
+
+      response.body
+    end
+
+    def create_member(space_id, role, attributes={})
+      response = Podio.connection.post do |req|
+        req.url "/space/#{space_id}/member/"
         req.body = attributes.merge(:role => role)
       end
 
@@ -49,8 +62,18 @@ class Podio::SpaceInvite < ActivePodio::Base
       response.body
     end
 
+    def decline_member(invite_code)
+      Podio.connection.delete("/space/membership?invite_code=#{invite_code}").status
+
+      response.body
+    end
+
     def find(invite_code)
       member Podio.connection.get("/space/invite/status?invite_code=#{ERB::Util.url_encode(invite_code)}").body
+    end
+
+    def find_member(invite_code)
+      member Podio.connection.get("/space/membership?invite_code=#{ERB::Util.url_encode(invite_code)}").body
     end
     
   end
