37bafbd206a6422e1f040948e8f58ce56bad8777
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2011-07-29 10:32:37 +0200

    Adding space search

--- lib/podio/models/search.rb
+++ lib/podio/models/search.rb
@@ -9,6 +9,15 @@ class Podio::Search < ActivePodio::Base
 
       list response.body
     end    
+
+    def in_space(space_id, words)
+      response = Podio.connection.post do |req|
+        req.url "/search/space/#{space_id}/"
+        req.body = words
+      end
+
+      list response.body
+    end    
   end
   
 end
