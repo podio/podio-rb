30829bc4eec6131f35fe867d54d8b3482c42e0a4
Florian Munz <surf@theflow.de>
2011-08-10 15:30:04 +0200

    verified_on property is gone

--- lib/podio/models/user_mail.rb
+++ lib/podio/models/user_mail.rb
@@ -2,9 +2,5 @@ class Podio::UserMail < ActivePodio::Base
   property :mail, :string
   property :verified, :boolean
   property :primary, :boolean
-  property :verified_on, :datetime
 
-  class << self
-
-  end
 end
