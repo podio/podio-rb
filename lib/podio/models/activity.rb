31d87314114b4b9ab4050070cae8885ec8deefb8
Casper Fabricius <me@casperfabricius.com>
2011-08-11 15:22:53 +0200

    Added activity and stream object models

---
+++ lib/podio/models/activity.rb
@@ -0,0 +1,10 @@
+class Podio::Activity < ActivePodio::Base
+  property :id, :integer
+  property :type, :string
+  property :actvitity_type, :string
+  property :data, :hash
+  property :created_on, :datetime
+
+  has_one :created_by, :class => 'ByLine'
+  has_one :created_via, :class => 'Via'
+end
---
+++ lib/podio/models/stream_object.rb
@@ -0,0 +1,30 @@
+class Podio::StreamObject < ActivePodio::Base
+  property :id, :integer
+  property :type, :string
+  property :last_update_on, :datetime
+  property :title, :string
+  property :link, :string
+  property :rights, :array
+  property :data, :hash
+  property :comments_allowed, :boolean
+  property :user_ratings, :hash
+  property :created_on, :datetime
+
+  has_one :created_by, :class => 'ByLine'
+  has_one :created_via, :class => 'Via'
+  has_one :app, :class => 'Application'
+  has_one :space, :class => 'Space'
+  has_one :org, :class => 'Organization'
+  
+  has_many :comments, :class => 'Comment'
+  has_many :files, :class => 'FileAttachment'
+  has_many :activity, :class => 'Activity'
+
+  class << self
+    def find_all_by_user_id(user_id, options={})
+      list Podio.connection.get { |req|
+        req.url("/stream/user/#{user_id}/", options)
+      }.body      
+    end
+  end
+end
