c551015a98e41eb548ce1a3cd6d50362e0d50042
Casper Fabricius <me@casperfabricius.com>
2012-02-27 10:45:35 +0100

    Removed inclusion of ActiveSupport::Concern from Updateable as it is not needed and generates warnings in Rails 3.2.1

--- lib/podio/active_podio/updatable.rb
+++ lib/podio/active_podio/updatable.rb
@@ -1,22 +1,17 @@
 # Adds functionality related to updating to an +ActivePodio+ model
 module ActivePodio
   module Updatable
-    extend ActiveSupport::Concern
-  
-    module InstanceMethods
-      def update_attributes(attributes)
-        attributes.each do |key, value|
-          self.send("#{key}=".to_sym, value.presence)
-        end
-      end
-
-      def remove_nil_values(input_hash)
-        input_hash.inject({}) do |hash, (key, value)|
-          hash[key] = value if value.present?
-          hash
-        end
+    def update_attributes(attributes)
+      attributes.each do |key, value|
+        self.send("#{key}=".to_sym, value.presence)
       end
     end
-  
+
+    def remove_nil_values(input_hash)
+      input_hash.inject({}) do |hash, (key, value)|
+        hash[key] = value if value.present?
+        hash
+      end
+    end
   end
 end
