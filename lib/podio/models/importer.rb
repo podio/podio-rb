667e5d292c4299e80aa2bd59d4e3130474786970
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2012-03-12 14:52:59 -0700

    Use options for importer

--- lib/podio/models/importer.rb
+++ lib/podio/models/importer.rb
@@ -1,14 +1,10 @@
 class Podio::Importer < ActivePodio::Base
-  
+
   class << self
-    def process_file(file_id, app_id, mappings, tags_column_id)
+    def process_file(file_id, options = {})
       response = Podio.connection.post do |req|
         req.url "/importer/#{file_id}/process"
-        req.body = {
-          :app_id => app_id,
-          :mappings => mappings,
-          :tags_column_id => tags_column_id
-        }
+        req.body = options
       end
 
       response.body
@@ -17,6 +13,6 @@ class Podio::Importer < ActivePodio::Base
     def get_columns(file_id)
       list Podio.connection.get("/importer/#{file_id}/column/").body
     end
-    
+
   end
 end
