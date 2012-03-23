bf5d13463834d0dcf2e9c02eb98d2afb5359150b
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2012-03-23 10:21:31 -0700

    Add space_id property to contacts

--- lib/podio/models/contact.rb
+++ lib/podio/models/contact.rb
@@ -1,11 +1,12 @@
 class Podio::Contact < Podio::Profile
   include ActivePodio::Updatable
-  
+
   property :user_id, :integer
   property :organization, :string
   property :role, :string # Only available when getting contacts for a space
   property :removable, :boolean # Only available when getting contacts for a space
   property :type, :string # user, space, connection - blank probably means it's a real user / Podio member
+  property :space_id, :integer
   property :link, :string
   property :last_seen_on, :datetime
   property :rights, :array
