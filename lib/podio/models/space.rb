a6261f4ef45f47650cf75c5359fa75b40a39c4c0
Casper Fabricius <me@casperfabricius.com>
2012-05-24 11:44:58 +0200

    Added delete instance method to Space model

--- lib/podio/models/space.rb
+++ lib/podio/models/space.rb
@@ -1,6 +1,6 @@
 class Podio::Space < ActivePodio::Base
   include ActivePodio::Updatable
-  
+
   property :space_id, :integer
   property :name, :string
   property :url, :string
@@ -17,21 +17,25 @@ class Podio::Space < ActivePodio::Base
   property :auto_join, :boolean
   property :type, :string
   property :premium, :boolean
-  
+
   has_one :created_by, :class => 'ByLine'
 
   alias_method :id, :space_id
-  
+
   def create
     response = Space.create(:org_id => org_id, :name => name, :privacy => self.privacy, :auto_join => self.auto_join)
     self.url = response['url']
     self.space_id = response['space_id']
   end
-  
+
   def update
     self.class.update(self.space_id, :name => self.name, :post_on_new_app => self.post_on_new_app, :post_on_new_member => self.post_on_new_member, :url_label => self.url_label, :privacy => self.privacy, :auto_join => self.auto_join)
   end
-  
+
+  def delete
+    self.class.delete(self.id)
+  end
+
   class << self
     def create(attributes)
       response = Podio.connection.post do |req|
@@ -53,7 +57,7 @@ class Podio::Space < ActivePodio::Base
     def find(id)
       member Podio.connection.get("/space/#{id}").body
     end
-    
+
     def join(space_id)
       Podio.connection.post("/space/#{space_id}/join").body
     end
@@ -61,7 +65,7 @@ class Podio::Space < ActivePodio::Base
     def find_by_url(url)
       member Podio.connection.get("/space/url?url=#{ERB::Util.url_encode(url)}").body
     end
-    
+
     def find_all_for_org(org_id)
       list Podio.connection.get("/org/#{org_id}/space/").body
     end
@@ -69,14 +73,14 @@ class Podio::Space < ActivePodio::Base
     def find_open_for_org(org_id)
       list Podio.connection.get("/space/org/#{org_id}/available/").body
     end
-    
+
     def validate_url_label(org_id, url_label)
       Podio.connection.post { |req|
         req.url "/space/org/#{org_id}/url/validate"
         req.body = {:url_label => url_label}
       }.body
     end
-    
+
   end
 end
 
