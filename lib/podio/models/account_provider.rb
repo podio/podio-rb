9e30be7bf0d368938ef666d0ac72b737c900410b
Kenneth Auchenberg <kenneth@auchenberg.dk>
2012-06-01 12:41:03 +0200

    Added AccountProvider model

---
+++ lib/podio/models/account_provider.rb
@@ -0,0 +1,16 @@
+class Podio::AccountProvider < ActivePodio::Base
+  property :name, :string
+  property :connect_link, :string
+  property :humanized_name, :string
+  property :capabilities, :array
+
+  class << self
+
+    def find_all(options = {})
+      list Podio.connection.get { |req|
+        req.url("/linked_account/provider", options)
+      }.body
+    end
+
+  end
+end
