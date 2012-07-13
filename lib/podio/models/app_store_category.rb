a59940cbac93e77f55ededb0908ae969bb838869
Kenneth Auchenberg <kenneth@auchenberg.dk>
2012-07-13 13:53:59 +0200

    Updated app store category

--- lib/podio/models/app_store_category.rb
+++ lib/podio/models/app_store_category.rb
@@ -1,15 +1,22 @@
 class Podio::AppStoreCategory < ActivePodio::Base
   property :category_id, :integer
   property :name, :string
+  property :type, :string
 
   alias_method :id, :category_id
 
   class << self
 
     def find_all
-      Podio.connection.get("/app_store/category/").body
-    end
+      categories = Podio.connection.get("/app_store/category/").body
 
+      categories.each do | key, value |
+        categories[key] = list value
+      end
+
+      categories
+
+    end
   end
 end
 
