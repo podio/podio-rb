f3ef22105be613b0799ff3ee8ae9c7908eb94c28
Casper Fabricius <me@casperfabricius.com>
2012-08-14 11:45:08 +0200

    Support options in the Category find_all call

--- lib/podio/models/category.rb
+++ lib/podio/models/category.rb
@@ -4,36 +4,38 @@ class Podio::Category < ActivePodio::Base
   property :name, :string
 
   alias_method :id, :category_id
-  
+
   class << self
     def create(attributes)
       response = Podio.connection.post do |req|
         req.url "/app_store/category/"
         req.body = attributes
       end
-    
+
       response.status
     end
-    
+
     def update(id, attributes)
       response = Podio.connection.put do |req|
         req.url "/app_store/category/#{id}"
         req.body = attributes
       end
-    
+
       response.status
     end
-    
+
     def delete(id)
       Podio.connection.delete("/app_store/category/#{id}").status
     end
-    
+
     def find(id)
       member Podio.connection.get("/app_store/category/#{id}").body
     end
-    
-    def find_all
-      collection Podio.connection.get("/app_store/category/").body
+
+    def find_all(options = {})
+      collection Podio.connection.get { |req|
+        req.url("/app_store/category/", options)
+      }.body
     end
 
     private
