ab4f70561033a0e3de9c2d8a2b21b59ac46557b7
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2012-09-07 15:17:57 -0700

    Change global stream method to /stream/

--- lib/podio/models/stream_object.rb
+++ lib/podio/models/stream_object.rb
@@ -15,7 +15,7 @@ class Podio::StreamObject < ActivePodio::Base
   has_one :app, :class => 'Application'
   has_one :space, :class => 'Space'
   has_one :org, :class => 'Organization'
-  
+
   has_many :comments, :class => 'Comment'
   has_many :files, :class => 'FileAttachment'
   has_many :activity, :class => 'Activity'
@@ -25,42 +25,42 @@ class Podio::StreamObject < ActivePodio::Base
   class << self
     def find_all(options={})
       list Podio.connection.get { |req|
-        req.url("/stream/v2/", options)
-      }.body      
+        req.url("/stream/", options)
+      }.body
     end
 
     def find_all_by_space_id(space_id, options={})
       list Podio.connection.get { |req|
         req.url("/stream/space/#{space_id}/v2/", options)
-      }.body      
+      }.body
     end
 
     def find_all_by_org_id(org_id, options={})
       list Podio.connection.get { |req|
         req.url("/stream/org/#{org_id}/v2/", options)
-      }.body      
+      }.body
     end
 
     def find_all_by_app_id(app_id, options={})
       list Podio.connection.get { |req|
         req.url("/stream/app/#{app_id}/", options)
-      }.body      
+      }.body
     end
 
     def find_all_by_user_id(user_id, options={})
       list Podio.connection.get { |req|
         req.url("/stream/user/#{user_id}/", options)
-      }.body      
+      }.body
     end
-    
+
     def find_by_ref(ref_type, ref_id)
       member Podio.connection.get("/stream/#{ref_type}/#{ref_id}/v2").body
     end
-    
+
     def find_all_personal(options={})
       list Podio.connection.get { |req|
         req.url("/stream/personal/", options)
-      }.body      
-    end    
+      }.body
+    end
   end
 end
