16e5dd59b5c94ae1d78806d429fb882c0b611b4d
holm <holm@podio.com>
2012-10-03 21:59:09 +0200

    Simplify this to match the changes in the API

--- lib/podio/models/stream_mute.rb
+++ lib/podio/models/stream_mute.rb
@@ -4,9 +4,6 @@ class Podio::StreamMute < ActivePodio::Base
   property :type, :string
   property :title, :string
   property :data, :hash
-  property :item, :boolean
-  property :status, :boolean
-  property :task, :boolean
 
   class << self
     # @see https://developers.podio.com/doc/stream/get-mutes-in-global-stream-62742
@@ -15,16 +12,14 @@ class Podio::StreamMute < ActivePodio::Base
     end
 
     # @see https://developers.podio.com/doc/stream/mute-object-from-global-stream-79424
-    def create(scope_type, scope_id, object_type = nil)
+    def create(scope_type, scope_id)
       path = "/stream/mute/#{scope_type}/#{scope_id}"
-      path += "/#{object_type}" unless object_type.nil?
       Podio.connection.post(path).status
     end
 
     # @see https://developers.podio.com/doc/stream/unmute-objects-from-the-global-stream-79426
-    def delete(scope_type, scope_id, object_type = nil)
+    def delete(scope_type, scope_id)
       path = "/stream/mute/#{scope_type}/#{scope_id}"
-      path += "/#{object_type}" unless object_type.nil?
       Podio.connection.delete(path).status
     end
   end
