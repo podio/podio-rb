3dd130ab4ff47c91474e082d69010750d93160d1
Casper Fabricius <me@casperfabricius.com>
2012-11-12 11:17:50 +0100

    Added new operation to Grant model

--- lib/podio/models/grant.rb
+++ lib/podio/models/grant.rb
@@ -18,6 +18,7 @@ class Podio::Grant < ActivePodio::Base
   handle_api_errors_for :save
 
   class << self
+    # https://hoist.podio.com/api/item/16168841
     def create(ref_type, ref_id, attributes={})
       response = Podio.connection.post do |req|
         req.url "/grant/#{ref_type}/#{ref_id}"
@@ -27,6 +28,7 @@ class Podio::Grant < ActivePodio::Base
       response.body
     end
 
+    # https://hoist.podio.com/api/item/16490748
     def find_own(ref_type, ref_id)
       response = Podio.connection.get("/grant/#{ref_type}/#{ref_id}/own")
       if response.status == 200
@@ -36,20 +38,29 @@ class Podio::Grant < ActivePodio::Base
       end
     end
 
+    # https://hoist.podio.com/api/item/16491464
     def find_all(ref_type, ref_id)
       list Podio.connection.get("grant/#{ref_type}/#{ref_id}/").body
     end
 
+    # https://hoist.podio.com/api/item/16496711
     def delete(ref_type, ref_id, user_id)
       Podio.connection.delete("grant/#{ref_type}/#{ref_id}/#{user_id}").body
     end
 
+    # https://hoist.podio.com/api/item/19275931
     def count_by_reference(ref_type, ref_id)
       Podio.connection.get("/grant/#{ref_type}/#{ref_id}/count").body['count']
     end
 
+    # https://hoist.podio.com/api/item/19389786
     def find_for_user_on_space(space_id, user_id)
       list Podio.connection.get("/grant/space/#{space_id}/user/#{user_id}/").body
+    end
+
+    # https://hoist.podio.com/api/item/22330891
+    def find_own_on_org(org_id)
+      list Podio.connection.get("/grant/org/#{org_id}/own/").body
     end
 
   end
