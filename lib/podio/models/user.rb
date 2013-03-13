f7c744e3bbdef46915428a0b60f0d790c5247869
Christian Holm <holm@podio.com>
2013-03-13 07:33:38 +0100

    Remove old betas property now on the user status

--- lib/podio/models/user.rb
+++ lib/podio/models/user.rb
@@ -9,7 +9,6 @@ class Podio::User < ActivePodio::Base
   property :old_password, :string
   property :new_password, :string
   property :flags, :array
-  property :betas, :array
   property :created_on, :datetime
   property :name, :string
   property :link, :string
