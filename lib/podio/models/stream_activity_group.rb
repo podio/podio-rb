b8e70d4fefa5ed7b4c34ccf9b54ad0a3e462a351
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2013-10-23 13:55:22 -0700

    Add method for getting group by data ref

--- lib/podio/models/stream_activity_group.rb
+++ lib/podio/models/stream_activity_group.rb
@@ -1,6 +1,7 @@
 # @see https://developers.podio.com/doc/stream
 class Podio::StreamActivityGroup < ActivePodio::Base
 
+  property :group_id, :integer
   property :kind, :string
   property :type, :string
   property :created_on, :datetime
@@ -12,11 +13,19 @@ class Podio::StreamActivityGroup < ActivePodio::Base
 
   has_many :authors, :class => 'ByLine'
 
+  alias_method :id, :group_id
+
   class << self
 
     def find_by_ref(ref_type, ref_id, options = {})
       list Podio.connection.get { |req|
         req.url("/stream/#{ref_type}/#{ref_id}/activity_group", options)
+      }.body
+    end
+
+    def find_for_data_ref(ref_type, ref_id, data_ref_type, data_ref_id)
+      member Podio.connection.get { |req|
+        req.url("/stream/#{ref_type}/#{ref_id}/activity_group/#{data_ref_type}/#{data_ref_id}")
       }.body
     end
 
