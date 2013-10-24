9be4971b82f23014978b53a2e0c82a7a80bc5540
Christian Holm <holm@podio.com>
2013-10-24 14:13:03 +0200

    Add method to block a contract

--- lib/podio/models/contract.rb
+++ lib/podio/models/contract.rb
@@ -95,6 +95,10 @@ class Podio::Contract < ActivePodio::Base
     self.class.change_to_variable(self.contract_id)
   end
 
+  def block
+    self.class.block(self.contract_id)
+  end
+
   def unblock
     self.class.unblock(self.contract_id)
   end
@@ -196,6 +200,11 @@ class Podio::Contract < ActivePodio::Base
 
     def change_to_variable(contract_id)
       response = Podio.connection.post("/contract/#{contract_id}/change_to/variable")
+      response.status
+    end
+
+    def block(contract_id)
+      response = Podio.connection.post("/contract/#{contract_id}/block")
       response.status
     end
 
