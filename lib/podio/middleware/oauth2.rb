7f4eed5c195b1b1aa971abfcd1ebde696e880c37
Rainer Kuhn <ticktricktrack@gmail.com>
2014-07-08 13:25:37 +0100

    Get client via Podio.client, which in turn pulls it out of Thread.current. That way the client doesn't have to be passed through the faraday request.

--- lib/podio/middleware/oauth2.rb
+++ lib/podio/middleware/oauth2.rb
@@ -4,16 +4,14 @@ module Podio
   module Middleware
     class OAuth2 < Faraday::Middleware
       def call(env)
-        podio_client = env[:request][:oauth]
         orig_env = env.dup
-
         begin
           @app.call(env)
         rescue TokenExpired
-          podio_client.refresh_access_token
+          Podio.client.refresh_access_token
 
           # new access token needs to be put into the header
-          orig_env[:request_headers].merge!(podio_client.configured_headers)
+          orig_env[:request_headers].merge!(Podio.client.configured_headers)
 
           # rewind the body if this was a file upload
           orig_env[:body].rewind if orig_env[:body].respond_to?(:rewind)
