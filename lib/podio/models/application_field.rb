90fdc222bac2d3c58b5d11728c884e55f48d09a8
Pieterjan Muller <pjmuller@gmail.com>
2014-09-03 22:23:41 +0200

    Added create and update method for Podio::ApplicationField

--- lib/podio/models/application_field.rb
+++ lib/podio/models/application_field.rb
@@ -17,5 +17,15 @@ class Podio::ApplicationField < ActivePodio::Base
       member Podio.connection.get("/app/#{app_id}/field/#{field_id}").body
     end
 
+    # @see https://developers.podio.com/doc/applications/update-an-app-field-22356
+    def update(app_id, field_id, attributes)
+      Podio.connection.put("/app/#{app_id}/field/#{field_id}", attributes).status
+    end
+
+    # @see https://developers.podio.com/doc/applications/add-new-app-field-22354
+    def create(app_id, attributes)
+      Podio.connection.post("/app/#{app_id}/field/", attributes).status
+    end
+
   end
 end
