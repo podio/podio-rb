d3b0ae810b2ee63ef13fe2fa86ceb9f8b5c69cfc
Christian Holm <holm@podio.com>
2014-12-19 22:54:35 +0100

    Add new price attributes

--- lib/podio/models/contract_price.rb
+++ lib/podio/models/contract_price.rb
@@ -3,9 +3,21 @@ class Podio::ContractPrice < ActivePodio::Base
   has_one :employee, :class => 'ContractPriceItem'
   has_one :external, :class => 'ContractPriceItem'
   has_one :item,     :class => 'ContractPriceItem'
+  
+  def mrr_saved
+    self.employee.mrr_saved + self.external.mrr_saved
+  end
+  
+  def mrr_list
+    self.employee.mrr_list + self.external.mrr_list
+  end
+
+  def mrr
+    self.employee.mrr + self.external.mrr
+  end
 
   def total
-    self.employee.sub_total + self.external.sub_total
+    self.mrr
   end
 
   class << self
@@ -22,15 +34,16 @@ class Podio::ContractPrice < ActivePodio::Base
 end
 
 class Podio::ContractPriceItem < ActivePodio::Base
+  property :list_price, :float #The list price per user
   property :price, :float # Price per user
+  property :price_rebate, :float # The rebate on the price
   property :quantity, :integer # Number of users
+  property :mrr_saved, :float #The MRR saved 
+  property :mrr_list, :float #The MRR based on the list price
+  property :mrr, :float #The MRR on the rebated price
 
   def sub_total
-    if self.quantity
-      (self.quantity*self.price).to_f
-    else
-      0.to_f
-    end
+    mrr
   end
 
 end
