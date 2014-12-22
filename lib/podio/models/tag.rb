69ce563ac23bd0eef1afe58ef62b7f35e2577315
Pieterjan Muller <pjmuller@gmail.com>
2014-12-22 10:08:26 +0100

    additional tag methods

--- lib/podio/models/tag.rb
+++ lib/podio/models/tag.rb
@@ -35,5 +35,42 @@ class Podio::Tag < ActivePodio::Base
       text = CGI.escape(text) if text
       Podio.connection.get("/tag/app/#{app_id}/top/?limit=#{limit}&text=#{text}").body
     end
+
+    # @see https://developers.podio.com/doc/tags/get-tags-on-space-22466
+    def find_by_space(space_id, limit, text)
+      text = CGI.escape(text) if text
+      list Podio.connection.get("/tag/space/#{space_id}/?limit=#{limit}&text=#{text}").body
+    end
+
+    # @see https://developers.podio.com/doc/tags/get-tags-on-organization-48473
+    def find_by_org(org_id, limit, text)
+      text = CGI.escape(text) if text
+      list Podio.connection.get("/tag/org/#{org_id}/?limit=#{limit}&text=#{text}").body
+    end
+
+    # @see https://developers.podio.com/doc/tags/get-objects-on-app-with-tag-22469
+    def find_for_app(app_id, attributes)
+      list Podio.connection.get("/tag/app/#{app_id}/search/", attributes).body
+    end
+
+    # @see https://developers.podio.com/doc/tags/get-objects-on-space-with-tag-22468
+    def find_for_space(space_id, attributes)
+      list Podio.connection.get("/tag/space/#{space_id}/search/", attributes).body
+    end
+
+    # @see https://developers.podio.com/doc/tags/get-objects-on-organization-with-tag-48478
+    def find_for_org(org_id, attributes)
+      list Podio.connection.get("/tag/org/#{org_id}/search/", attributes).body
+    end
+
+    # @see https://developers.podio.com/doc/tags/remove-tag-22465
+    def delete(tagable_type, tagable_id, attributes)
+      response = Podio.connection.delete do |req|
+        req.url "/tag/#{tagable_type}/#{tagable_id}/"
+        req.body = attributes
+      end
+
+      response.body
+    end
   end
 end
