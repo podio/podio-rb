56723462351bfa44831f95c0abb1682e347a1b3a
Albert Fernández <albertfdp@gmail.com>
2015-08-11 15:58:43 +0200

    add support for finding contract attribution

--- lib/podio/models/contract_attribution.rb
+++ lib/podio/models/contract_attribution.rb
@@ -2,11 +2,15 @@ class Podio::ContractAttribution < ActivePodio::Base
 
   property :contract_id, :integer
   property :name, :string
-  property :attribution_id, :string
+  property :partner_id, :string
 
   alias_method :id, :contract_id
 
   class << self
+
+    def find(contract_id, options={})
+      member Podio.connection.get("contract/attribution/#{contract_id}", options).body
+    end
 
     def update(contract_id, partner_id)
       member Podio.connection.put { |req|
