e266440249bddd6cc4402e1ea88507d8749adec9
Ernest Surudo <ernest.surudo@gmail.com>
2019-09-22 14:58:30 +0200

    Remove use of `show_feature?` method, since it isn't defined anywhere

--- lib/podio/client.rb
+++ lib/podio/client.rb
@@ -107,18 +107,16 @@ module Podio
     end
 
     # Sign in with SAML SSO
-    if show_feature?(:sso_saml)
-      def authenticate_with_saml(attributes)
-        response = @oauth_connection.post do |req|
-          req.url '/oauth/token', :grant_type => 'saml', :client_id => SETTINGS.podio_api.default_client.api_key, :client_secret => SETTINGS.podio_api.default_client.api_secret
-          req.body = attributes
-        end
-        @oauth_token = OAuthToken.new(response.body)
-        configure_oauth
-        @oauth_token
+    def authenticate_with_saml(attributes)
+      response = @oauth_connection.post do |req|
+        req.url '/oauth/token', :grant_type => 'saml', :client_id => SETTINGS.podio_api.default_client.api_key, :client_secret => SETTINGS.podio_api.default_client.api_secret
+        req.body = attributes
       end
+      @oauth_token = OAuthToken.new(response.body)
+      configure_oauth
+      @oauth_token
     end
-    
+
     # Sign in with an OpenID, only available for Podio
     def authenticate_with_openid(identifier, type)
       response = @trusted_connection.post do |req|
