d542e8262932fed4248043a0263d4e63acaace0c
Prateek Gupta <gprat2108@gmail.com>
2020-01-30 18:36:02 +0530

    filter_values hash of array[ActionController Parameter] converted to hash of array of id.

--- lib/podio/models/item.rb
+++ lib/podio/models/item.rb
@@ -140,7 +140,9 @@ class Podio::Item < ActivePodio::Base
 
     # @see https://developers.podio.com/doc/items/filter-items-4496747
     def find_by_filter_values(app_id, filter_values, attributes={}, options={})
-      attributes[:filters] = filter_values
+      # filter_values are now coming as hash of ActionController Parameter,
+      # so tranforming its values to have array of id's only
+      attributes[:filters] = filter_values.transform_values{|v|  v.map{|e| e["id"]}}
       collection Podio.connection.post { |req|
         req.url("/item/app/#{app_id}/filter/", options)
         req.body = attributes
